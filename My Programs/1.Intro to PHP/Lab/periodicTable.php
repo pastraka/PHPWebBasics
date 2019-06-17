551e6533b3547a7cc2a4bd64fc3b493f860500e9
Mihail Atanasov <mihailatanasov1986@gmail.com>
2019-06-17 13:09:44 +0100

    Lab13

---
+++ My Programs/1.Intro to PHP/Lab/periodicTable.php
@@ -0,0 +1,19 @@
+<?php
+
+$input = explode(", ", readline());
+$arr = [];
+
+
+for ($i = 0; $i < count($input); $i++) {
+    if (!array_key_exists($input[$i], $arr)) {
+        $arr[$input[$i]] = 1;
+    } else {
+        $arr[$input[$i]] += 1;
+    }
+}
+
+foreach ($arr as $item => $counter) {
+    if ($counter == 1) {
+        echo ltrim($item, ", ")." ";
+    }
+}
