d9386381e878fad77cee48422982909eb98238d7
Mihail Atanasov <mihailatanasov1986@gmail.com>
2019-08-12 13:56:42 +0100

    Exercises

--- My Programs/1.Intro to PHP/Exercises/mostFrequentNumber.php
+++ My Programs/1.Intro to PHP/Exercises/mostFrequentNumber.php
@@ -1,2 +1,11 @@
 <?php
-
+$input = explode(" ", readline());
+$maxFreq = 0;
+$output = "";
+foreach (array_count_values($input) as $key => $value) {
+    if ($value > $maxFreq) {
+        $maxFreq = $value;
+        $output = $key;
+    }
+}
+echo $output;
