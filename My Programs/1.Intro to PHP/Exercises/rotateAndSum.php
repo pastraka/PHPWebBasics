f1bdba29e279484756e39db02e57c774e6bd2320
Mihail Atanasov <mihailatanasov1986@gmail.com>
2019-09-09 13:49:09 +0100

    Exercises

---
+++ My Programs/1.Intro to PHP/Exercises/rotateAndSum.php
@@ -0,0 +1,21 @@
+<?php
+
+$input = explode(" ", readline());
+$n = intval(readline());
+$arr = [];
+$result = [];
+
+for ($i = $n; $i > 0; $i--) {
+    $temp = array_pop($input);
+    array_unshift($input, $temp);
+    $arr[$i] = $input;
+    $length = count($arr[$i]);
+}
+
+for ($j = 0; $j < $length; $j++) {
+    $temp = array_sum(array_column($arr, $j));
+    array_unshift($result, $temp);
+}
+$result = array_reverse($result);
+
+echo implode(" ", $result);
