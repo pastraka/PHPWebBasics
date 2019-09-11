6e6ca7bc54da7016e133fbb62c6b2a14a78ac533
Mihail Atanasov <mihailatanasov1986@gmail.com>
2019-09-11 13:43:02 +0100

    exercises

---
+++ My Programs/1.Intro to PHP/Exercises/maximalSum.php
@@ -0,0 +1,62 @@
+<?php
+
+$input = explode(" ", readline());
+$rowSize = $input[0];
+$columnSize = $input[1];
+$matrix = [];
+
+for ($row = 0; $row < $rowSize; $row++) {
+    $matrix[$row] = array_map("intval", explode(" ", readline()));
+}
+
+$totalSum = 0;
+$initialRow = 0;
+$initialCol = 0;
+
+if ($rowSize % 2 == 0) {
+    for ($row = 0; $row < count($matrix) - 3; $row++) {
+        for ($col = 0; $col < count($matrix[$row]) - 3; $col++) {
+            $currentSum = $matrix[$row][$col]
+                        + $matrix[$row][$col + 1]
+                        + $matrix[$row][$col + 2]
+                        + $matrix[$row + 1][$col]
+                        + $matrix[$row + 1][$col + 1]
+                        + $matrix[$row + 1][$col + 2]
+                        + $matrix[$row + 2][$col]
+                        + $matrix[$row + 2][$col + 1]
+                        + $matrix[$row + 2][$col + 2];
+            if ($currentSum > $totalSum) {
+                $totalSum = $currentSum;
+                $initialRow = $row;
+                $initialCol = $col;
+            }
+        }
+    }
+} else {
+    for ($row = 0; $row < count($matrix) - 2; $row++) {
+        for ($col = 0; $col < count($matrix[$row]) - 2; $col++) {
+            $currentSum = $matrix[$row][$col]
+                        + $matrix[$row][$col + 1]
+                        + $matrix[$row][$col + 2]
+                        + $matrix[$row + 1][$col]
+                        + $matrix[$row + 1][$col + 1]
+                        + $matrix[$row + 1][$col + 2]
+                        + $matrix[$row + 2][$col]
+                        + $matrix[$row + 2][$col + 1]
+                        + $matrix[$row + 2][$col + 2];
+            if ($currentSum > $totalSum) {
+                $totalSum = $currentSum;
+                $initialRow = $row;
+                $initialCol = $col;
+            }
+        }
+    }
+}
+
+echo "Sum = " . $totalSum . PHP_EOL;
+for ($row = $initialRow; $row < $initialRow + 3; $row++) {
+    for ($col = $initialCol; $col < $initialCol + 3; $col++) {
+        echo $matrix[$row][$col] . " ";
+    }
+    echo PHP_EOL;
+}
