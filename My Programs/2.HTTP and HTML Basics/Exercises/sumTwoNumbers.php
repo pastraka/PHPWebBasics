1b391033d89743fd64e1c61ed8b5510e93aae1f6
Mihail Atanasov <mihailatanasov1986@gmail.com>
2019-10-23 12:52:17 +0100

    exercises

--- My Programs/2.HTTP and HTML Basics/Exercises/sumTwoNumbers.php
+++ My Programs/2.HTTP and HTML Basics/Exercises/sumTwoNumbers.php
@@ -1,13 +1,19 @@
 <form>
     <div>First Number:</div>
     <label>
-        <input type="number" name="num1" />
+        <input type="number" name="num1"/>
     </label>
     <div>Second Number:</div>
     <label>
-        <input type="number" name="num2" />
+        <input type="number" name="num2"/>
     </label>
-    <div><input type="submit" /></div>
+    <div><input type="submit"/></div>
 </form>
 
 <?php
+if (isset($_GET['num1']) && isset($_GET['num2'])) {
+    $n1 = $_GET['num1'];
+    $n2 = $_GET['num2'];
+    $result = $n1 + $n2;
+    echo "$n1 + $n2 = $result";
+}
