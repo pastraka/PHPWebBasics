9adb7eb31e3d6adc882d650f36d9bfad9495d8ea
Mihail Atanasov <mihailatanasov1986@gmail.com>
2019-10-23 12:35:47 +0100

    exercises

---
+++ My Programs/2.HTTP and HTML Basics/Exercises/dumpHTTPGetRequest.php
@@ -0,0 +1,22 @@
+<form>
+    <div>Name:</div>
+    <label>
+        <input type="text" name="personName"/>
+    </label>
+    <div>Age:</div>
+    <label>
+        <input type="number" name="age"/>
+    </label>
+    <div>Town:</div>
+    <label>
+        <select name="townId">
+            <option value="10">Sofia</option>
+            <option value="20">Varna</option>
+            <option value="30">Plodvid</option>
+        </select>
+    </label>
+    <div><input type="submit"/></div>
+</form>
+
+<?php
+var_dump($_GET);
---
+++ My Programs/2.HTTP and HTML Basics/Exercises/sumTwoNumbers.php
@@ -0,0 +1,13 @@
+<form>
+    <div>First Number:</div>
+    <label>
+        <input type="number" name="num1" />
+    </label>
+    <div>Second Number:</div>
+    <label>
+        <input type="number" name="num2" />
+    </label>
+    <div><input type="submit" /></div>
+</form>
+
+<?php
