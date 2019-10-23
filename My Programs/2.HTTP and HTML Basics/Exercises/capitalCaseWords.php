2a4ac27cf1451e67a28e223eca1f76d86c7b3048
Mihail Atanasov <mihailatanasov1986@gmail.com>
2019-10-23 19:44:58 +0100

    exercises

--- My Programs/2.HTTP and HTML Basics/Exercises/capitalCaseWords.php
+++ My Programs/2.HTTP and HTML Basics/Exercises/capitalCaseWords.php
@@ -1,17 +1,19 @@
 <?php
-$text = "";
 if (isset($_GET['text'])) {
     $text = $_GET['text'];
-    preg_match_all('/\b[A-Z]+\b/m', $text, $words);
+    preg_match_all('/\w+/', $text, $words);
     $words = $words[0];
-    echo implode(", ", $words);
+    $upperWords = array_filter($words, function ($word) {
+        return strtoupper($word) == $word;
+    });
+    echo implode(', ', $upperWords);
 }
 ?>
 
-<form>
-    <label>
-        <textarea rows="10" name="text"></textarea>
-    </label>
-    <br>
-    <input type="submit" value="Extract">
-</form>
+<!--<form>-->
+<!--    <label>-->
+<!--        <textarea rows="10" name="text"></textarea>-->
+<!--    </label>-->
+<!--    <br>-->
+<!--    <input type="submit" value="Extract">-->
+<!--</form>-->
