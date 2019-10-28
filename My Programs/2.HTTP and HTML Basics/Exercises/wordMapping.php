cd64c432d3043e2dee2ab31c78ade7cda5d05896
pastraka <mihailatanasov1986@gmail.com>
2019-10-28 13:51:59 +0000

    exercises

--- My Programs/2.HTTP and HTML Basics/Exercises/wordMapping.php
+++ My Programs/2.HTTP and HTML Basics/Exercises/wordMapping.php
@@ -1,24 +1,23 @@
 <?php
-if (isset($_GET['input']) && !empty(trim($_GET['input']))) {
-    $text = trim($_GET['input']);
-    preg_match_all("/[A-Za-z]+/", $text, $words);
+if (isset($_GET["input"]) && !empty(trim($_GET["input"]))) {
+    $text = trim($_GET["input"]);
+    preg_match_all("/[a-zA-Z]+/", $text, $words);
     $words = array_map("strtolower", $words[0]);
     $wordsCount = [];
     foreach ($words as $word) {
-        if (array_key_exists($words, $wordsCount)) {
-            $wordsCount [$word] = 0;
+        if (!array_key_exists($word, $wordsCount)) {
+            $wordsCount[$word] = 0;
         }
         $wordsCount[$word]++;
     }
-    echo table($wordsCount);
+    echo buildTable($wordsCount);
 }
-
-function table($items)
+function buildTable($items)
 {
-    $result = "<table border='2'>";
+    $output = "<table border='2'>";
     foreach ($items as $key => $val) {
-        $result .= "<tr><td>{$key}</td><td>{$val}</td></tr>";
+        $output .= "<tr><td>{$key}</td><td>{$val}</td></tr>";
     }
-    $result .= "</table>";
-    return $result;
+    $output .= "</table>";
+    return $output;
 }
