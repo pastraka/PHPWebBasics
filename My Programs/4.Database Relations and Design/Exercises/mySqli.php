b017f58e5c8463140925b84eeaca855fba204961
pastraka <mihailatanasov1986@gmail.com>
2019-11-16 10:02:09 +0000

    exercises

--- My Programs/4.Database Relations and Design/Exercises/mySqli.php
+++ My Programs/4.Database Relations and Design/Exercises/mySqli.php
@@ -1,12 +1,13 @@
 <?php
 $db = new mysqli('localhost', 'root', '', 'soft_uni');
+$db->set_charset('utf8');
 
 if ($db->connect_error) {
     echo $db->connect_error;
     exit;
 }
 
-$result = $db->query('SELECT ' . 'EMPLOYEE_ID, FIRST_NAME, SALARY FROM employees');
+$result = $db->query('SELECT' . 'EMPLOYEE_ID, FIRST_NAME, SALARY FROM employees');
 
 while ($row = $result->fetch_assoc()){
     echo "\n----------------------------------\n";
