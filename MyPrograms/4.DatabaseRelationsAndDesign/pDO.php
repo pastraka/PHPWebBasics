46a1831c58c0011e8bebeb1bc8b812e8602d8370
pastraka <mihailatanasov1986@gmail.com>
2019-11-21 13:14:52 +0000

    exercises

--- MyPrograms/4.DatabaseRelationsAndDesign/pDO.php
+++ MyPrograms/4.DatabaseRelationsAndDesign/pDO.php
@@ -1,24 +1,31 @@
 <?php
-try {
-    $db = new PDO('mysql:host=localhost;dbname=php_web_test', 'root', '');
-    $result = $db->query('SELECT * FROM php_web_test.categories', PDO::FETCH_ASSOC);
-
-    foreach ($result as $row) {
-        print_r($row);
-    }
-    $result = null;
-    $db = null;
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-}
+$db = new PDO('mysql:host=localhost;dbname=php_web_test', 'root', '');
 ?>
 
-<form method="post">
-    Username:<label>
-        <input type="text" name="username"/>
-    </label><br/>
-    Password:<label>
-        <input type="text" name="password"/>
-    </label><br/>
-    <input type="submit" value="Submit Query"/>
-</form>
+    <form method="post">
+        Username:<label>
+            <input type="text" name="username"/>
+        </label><br/>
+        Password:<label>
+            <input type="text" name="password"/>
+        </label><br/>
+        <input type="submit" value="Submit Query"/>
+    </form>
+
+<?php
+if (isset($_POST['username'], $_POST['password'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $query = "INSERT INTO users (username, password) VALUES (?, ?)";
+    echo $query;
+    password_hash($password, PASSWORD_ARGON2I);
+    $statement = $db->prepare($query);
+    $result = $statement->execute([$username, password_hash($password, PASSWORD_ARGON2I)]);
+//    if ($result) {
+//        header("Location: login.php");
+//        exit;
+//    } else {
+//        echo "Error!";
+//    }
+}
+?>
