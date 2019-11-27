5701bb5f02b0a59496ea38c5cf5a9ec5b2731a75
pastraka <mihailatanasov1986@gmail.com>
2019-11-27 12:56:08 +0000

    exercises

--- MyPrograms/5.workShopProjectForum/workShopProjectForum/templates/questions_by_category.php
+++ MyPrograms/5.workShopProjectForum/workShopProjectForum/templates/questions_by_category.php
@@ -7,20 +7,19 @@
 <body>
 <?php include_once 'logged_in_header.php'; ?>
 
-<a href="<?= url("categories.php?"); ?>">Back to questions all categories</a>
+<a href="<?= url("categories.php?"); ?>">Back to all categories</a>
 |
 <a href="<?= url("ask_question.php?category_id=$id"); ?>">Ask new question</a>
-<hr/>
 <?php foreach ($questions as $question): ?>
     <hr/>
 
     <div class="question">
     <span>
         <a href="<?= url("question.php?id={$question['id']}"); ?>">
-        <?= htmlspecialchars($question['title']); ?>
+            <?= htmlspecialchars($question['title']); ?>
         </a>
     </span>
-        <span><?= ($question['answers_count']); ?></span>
+        <span> (<?= $question['answers_count']; ?>)</span>
         <br/>
         <span><?= htmlspecialchars($question['author_name']); ?></span>
         <span><?= $question['created_on']; ?></span>
@@ -28,13 +27,10 @@
     </div>
     <div>
         <?php if (hasLiked($db, $userId, $question['id'])): ?>
-            <a href="<?= url("category.php?id={$_GET['id']} &action=removeLike&question_id={$question['id']}");
-            ?>">Remove (<?= $question['likes_count']; ?>)
-                like</a>
+            <a href="<?= url("category.php?id={$_GET['id']}&action=removeLike&question_id={$question['id']}"); ?>">Remove
+                like</a> (<?= $question['likes_count']; ?>)
         <?php else: ?>
-            <a href="<?= url("category.php?id={$_GET['id']} &action=like&question_id={$question['id']}");
-            ?>">Like</a> (<?= $question['likes_count']; ?>)
-
+            <a href="<?= url("category.php?id={$_GET['id']}&action=like&question_id={$question['id']}"); ?>">Like</a> (<?= $question['likes_count']; ?>)
         <?php endif; ?>
     </div>
 <?php endforeach; ?>
