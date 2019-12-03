28d2ab774361d4efcc35e41d5f4f08ee5bb64b97
pastraka <mihailatanasov1986@gmail.com>
2019-12-03 11:36:12 +0000

    exercises

--- MyPrograms/6.DefiningClasses/Lab/symmetryCheck.php
+++ MyPrograms/6.DefiningClasses/Lab/symmetryCheck.php
@@ -1,4 +1,16 @@
 <?php
+
+//$str = readline();
+//function isPalindrome($str)
+//{
+//    return $str === strrev($str);
+//}
+//if (isPalindrome($str)) {
+//    echo "true" . PHP_EOL;
+//} else {
+//    echo "false" . PHP_EOL;
+//}
+
 $str = readline();
 
 function isPalindrome($str)
