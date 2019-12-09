5f8150d3258af9c33f29ce5c66bfe331ed78118c
pastraka <mihailatanasov1986@gmail.com>
2019-12-09 20:29:53 +0000

    exercises

---
+++ MyPrograms/6.DefiningClasses/Exercises/opinionPoll.php
@@ -0,0 +1,58 @@
+<?php
+
+class Person
+{
+    private $name;
+    private $age;
+
+    /**
+     * Person constructor.
+     * @param $name
+     * @param $age
+     */
+    public function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    public function __toString()
+    {
+        return $this->getName() . " - " . $this->getAge();
+    }
+}
+
+$n = readline();
+$people = [];
+
+for ($i = 0; $i < $n; $i++) {
+    $persons = readline();
+    list($name, $age) = explode(" ", $persons);
+    if ($age > 30) {
+        $people[] = new Person($name, $age);
+    }
+}
+
+usort($people, function ($a, $b) {
+    return $a->getName() <=> $b->getName();
+});
+
+foreach ($people as $person) {
+    echo $person . PHP_EOL;
+}
