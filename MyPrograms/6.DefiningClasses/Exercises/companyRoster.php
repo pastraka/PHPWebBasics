de0c44fd98fe27a1fbc8b585c6b33681698ae914
pastraka <mihailatanasov1986@gmail.com>
2019-12-11 16:58:59 +0000

    exercises

---
+++ MyPrograms/6.DefiningClasses/Exercises/companyRoster.php
@@ -0,0 +1,128 @@
+<?php
+
+class Employee
+{
+    private string $name;
+    private float $salary;
+    private string $position;
+    private string $department;
+    private string $email;
+    private int $age;
+
+    /**
+     * Employee constructor.
+     * @param string $name
+     * @param float $salary
+     * @param string $position
+     * @param string $department
+     * @param string $email
+     * @param int $age
+     */
+    public function __construct(string $name, float $salary, string $position, string $department, string $email, int $age)
+    {
+        $this->name = $name;
+        $this->salary = $salary;
+        $this->position = $position;
+        $this->department = $department;
+        $this->email = $email;
+        $this->age = $age;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return float
+     */
+    public function getSalary(): float
+    {
+        return $this->salary;
+    }
+
+    /**
+     * @param float $salary
+     */
+    public function setSalary(float $salary): void
+    {
+        $this->salary = $salary;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPosition(): string
+    {
+        return $this->position;
+    }
+
+    /**
+     * @param string $position
+     */
+    public function setPosition(string $position): void
+    {
+        $this->position = $position;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDepartment(): string
+    {
+        return $this->department;
+    }
+
+    /**
+     * @param string $department
+     */
+    public function setDepartment(string $department): void
+    {
+        $this->department = $department;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAge(): int
+    {
+        return $this->age;
+    }
+
+    /**
+     * @param int $age
+     */
+    public function setAge(int $age): void
+    {
+        $this->age = $age;
+    }
+}
+
+$n = readline();
